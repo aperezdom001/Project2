12aeb7e753534b14e9d108fe69dbc72c748c28e6
aperezdom <aideperz@gmail.com>
2021-04-09 11:52:38 -0600

    Code to display my artists json and attempting to write code to random generate

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,11 @@
+import artistOfDay from './artistOfDay.json';
+import artPrompts from './artPrompts.json';
+import funFacts from './funFacts.json';
+
 export default function Home (props){
+  console.log(artistOfDay);
+  console.log(artPrompts);
+  console.log(funFacts);
  
     return(
         <div>
@@ -13,15 +20,40 @@ export default function Home (props){
                 use that to your advantage in your daily life.
             </h3>
 
+            <div>
             <h3>
                 Here in ARTDaily, learn some interesting fun facts
                 and check out the Artist of the Day along with an
                 art prompt!
             </h3>
 
-            <h4>
-                
-            </h4>
+            {artistOfDay.map((artist, i) => {
+                return(
+                    <div className='artistOfDayDisplay' key={i}>
+                        <h4>
+                            {artist.nameDate}
+                            {/* insert random generator code here */}
+                             {/* {Math.random({artist.name}) * artistOfDay.length} */}
+                        </h4>
+                        <h4>
+                            {artist.typeofArt}
+                             {/* insert random generator code here */}
+                             {/* {Math.random({artist.typeOfArt}) * artistOfDay.length} */}
+                        </h4>
+                        <h4>
+                            {artist.artwork}\ 
+                            {/* insert random generator code here */}
+                             {/* {Math.random({artist.artwork}) * artistOfDay.length} */}
+                            
+                        </h4>
+                    </div>  
+
+                )
+            })}
+
+            </div>
+           
+           
         </div>
     )
 }
