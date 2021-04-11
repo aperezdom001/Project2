d7c49eaa7931b8bf3d838808540af4350cabf94c
aperezdom <aideperz@gmail.com>
2021-04-11 15:28:10 -0600

    Expirement with creating grids using flex

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,67 +4,69 @@ import funFacts from './funFacts.json';
 import './Home.css';
 
 export default function Home (props){
-  console.log(artistOfDay);
-  console.log(artPrompts);
-  console.log(funFacts);
-
+ 
+// The variables random generator for the Home Page in artist of day, fun facts, and prompts //
   const randomIdx = Math.floor(Math.random()* artistOfDay.length-1);
-  console.log(artistOfDay[randomIdx]);
-
   const randomFactIdx = Math.floor(Math.random() * funFacts.length-1);
-
   const randomPromptIdx = Math.floor(Math.random() * artPrompts.length-1);
-  
  
     return(
         <div>
             <h1 className="home-page-title">~ StArt Home ~</h1>
-        
 
             <div className='border-wrap-home'>
-            <p className='greeting-intro'>
-                Explore the history of art, learn more about
-                artists, and observe new art techniques and concepts.
-               
-            </p>
-                
-            <p className='greeting-intro'>
-                Improve your creative side and learn how you can 
-                use that to your advantage in your daily life.
-            </p>
-            <p className='greeting-intro'>
-                Here in ARTDaily, learn some interesting fun facts
-                and check out the Artist of the Day along with an
-                art prompt!
-            </p>
 
-            
-             {artistOfDay[randomIdx] !== undefined && 
+                <p className='greeting-intro'>
+                    Explore the history of art, learn more about
+                    artists, and observe new art techniques and concepts.
+               
+                </p>
+                
+                 <p className='greeting-intro'>
+                    Improve your creative side and learn how you can 
+                    use that to your advantage in your daily life.
+                </p>
+
+                 <p className='greeting-intro'>
+                     To start the day with some art: 
+                 </p>
+
+                 <li className='list-start'> 1. Discover a new artist</li>
+                 <li className='list-start'> 2. Learn some fun facts</li>
+                 <li className='list-start'> 3. Do an art prompt!</li>
+
+                {/* The random generator variables being called with the json files I created for them */}
+                {artistOfDay[randomIdx] !== undefined && 
              
-                    <div>
-                        <h2 className='artist-day-title'>~ Artist of the Day ~</h2>
-                        <h4 className='artist-of-day'>
+                    <div className='home-art-fun-prompt-flex-container'>
+                        <h2 className='art-fun-prompt'>~ Artist of the Day ~</h2>
+
+                        <h4 className='art-fun-prompt'>
                             {artistOfDay[randomIdx].nameDate}
                         </h4>
-                        <h5 className='artist-of-day-type'>
+
+                        <h5 className='art-fun-prompt'>
                             {artistOfDay[randomIdx].typeofArt}
                         </h5>
-                        <h5 className='artist-of-day-artwork'>
+
+                        <h5 className='art-fun-prompt'>
                             {artistOfDay[randomIdx].artwork}
                         </h5>
+
                     </div>  
                         }
 
                     <div>
-                       
-                       <h2 className='fun-fact-title'>~ Fun Fact ~</h2>
-                       <h3 className='fun-facts'> {funFacts[randomFactIdx] !== undefined && funFacts[randomFactIdx].fact}</h3>
+                       <h2 className='art-fun-prompt'>~ Fun Fact ~</h2>
 
-                        </div>
+                       <h3 className='art-fun-prompt'> {funFacts[randomFactIdx] !== undefined && funFacts[randomFactIdx].fact}</h3>
+
+                    </div>
 
                     <div>
-                        <h2 className='art-prompt-title'>~ Prompt of the Day ~</h2>
-                        <h3 className='art-prompt'>{artPrompts[randomPromptIdx] !== undefined && artPrompts[randomPromptIdx].prompt}</h3>
+                        <h2 className='art-fun-prompt'>~ Prompt of the Day ~</h2>
+
+                        <h3 className='art-fun-prompt'>{artPrompts[randomPromptIdx] !== undefined && artPrompts[randomPromptIdx].prompt}</h3>
 
                     </div>
 
--- src/Components/Media/MediaDetail.js
+++ src/Components/Media/MediaDetail.js
@@ -17,7 +17,6 @@ export default function Media (props) {
         getMediaInfo();
     }, []);
 
-    console.log(mediaInfo);
 
     return(
         <div>
